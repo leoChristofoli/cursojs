8af1f57c03c33c1b91c4740cb2dfa16117bb8a72
Leonardo <leochp@gmail.com>
2018-07-23 15:33:16 -0300

    aula 3.17

---
+++ 3-Fundamentos/17-Numeros.js
@@ -0,0 +1,11 @@
+// Alguns cuidados
+
+console.log(7 / 0) // Retorna Infinito
+
+console.log('10' / 2)
+
+console.log('a' * 3) // NaN Not a Number
+
+console.log(0.7 + 0.1) // não é 0.8
+
+// console.log(10.toString()) // não funciona
