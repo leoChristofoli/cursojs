b433ad331600a9d42b9a6155f81a5560dc2a8515
leonardo <leochp@gmail.com>
2018-07-28 20:13:57 -0300

    aula 27

---
+++ 3-Fundamentos/27-Funcoes2.js
@@ -0,0 +1,15 @@
+// funçoes em variáveis
+const soma1 = function (a, b) {
+    return a + b
+}
+
+// função arrow
+const soma2 = (a, b) => {
+    return a + b
+}
+
+// retorno implícito
+
+const soma3 = (a, b) => a + b
+
+console.log(soma3(1, 2))
