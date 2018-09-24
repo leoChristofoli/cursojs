0fecf2f60c4bd24a6d6cf87733d8ba2c69499b93
Leonardo <leochp@gmail.com>
2018-09-23 21:13:27 -0300

    aula 36

---
+++ 3-Fundamentos/36-FuncVSObj.js
@@ -0,0 +1,8 @@
+console.log(typeof Object)
+console.log(typeof new Object())
+
+const Cliente = function(){}
+
+class Produto {} //ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto)
