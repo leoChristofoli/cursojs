bf844dd32ffbad32cf9a4dcf4d1205029c6ec567
a487czz <leochp@gmail.com>
2018-10-03 13:33:57 -0300

    aula 342

--- 20-React/exercicios/src/index.js
+++ 20-React/exercicios/src/index.js
@@ -4,6 +4,16 @@ import ReactDOM from 'react-dom'
 // import Primeiro from './components/Primeiro' // Todos os componentes devem iniciar com letra maiúscula
 // Primeiro é um alias
 
-import Welcome from './components/Welcome'
+import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
-ReactDOM.render(<Welcome nome="Leonardo" />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva" > 
+            <Filho nome="João" sobrenome="Silva" />
+        </Pai>
+    </div>
+
+, document.getElementById('root')
+)
