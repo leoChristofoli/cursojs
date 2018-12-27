a5a85c1fc09d82f9eadd39760e71366a3786a806
Leonardo Christofoli Pereira <leocp@br.ibm.com>
2018-12-27 10:28:55 -0200

    react start

--- 20-React/exercicios/src/index.js
+++ 20-React/exercicios/src/index.js
@@ -1,21 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import Primeiro from './components/Primeiro' // Todos os componentes devem iniciar com letra maiúscula
-// Primeiro é um alias
-
-import Saudacao from './components/Saudacao'
-import Pai from './components/Pai'
-import Filho from './components/Filho'
-
-ReactDOM.render(
-    <div>
-        <Pai nome="Paulo" sobrenome="Silva" > 
-            <Filho nome="João" sobrenome="Silva" />
-            <Filho nome="Carla" sobrenome="Pereira" />
-            <Filho nome="Paulo" sobrenome="Pereira" />
-        </Pai>
-    </div>
-
-, document.getElementById('root')
-)
+ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
